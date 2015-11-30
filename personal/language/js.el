769ae31057454a58e725efd7d9ea6d9f50fec458
hadwinzhy <hadwinzhy@gmail.com>
2015-11-30 15:21:19 +0800

    add ejs & react config

--- personal/language/js.el
+++ personal/language/js.el
@@ -6,7 +6,19 @@
 ;;; Code:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Enhance JS2 Mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 '(js2-highlight-unused-variables-mode 1)
 '(js2-highlight-level 3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; JSX Mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 ;;; js.el ends here
--- personal/third-party-packages.el
+++ personal/third-party-packages.el
@@ -1,1 +1,12 @@
+;;; package --- Summary
+;;; Commentary:
+
+;;; Code:
+(prelude-require-package 'web-mode)
 (prelude-require-package 'js2-mode)
+;(prelude-require-package 'jsx-mode)
+(prelude-require-package 'react-snippets)
+
+(provide 'third-party-packages)
+
+;;; third-party-packages.el ends here
